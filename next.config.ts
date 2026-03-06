649df53ec9f15fbf1af446c7a92a56c369d4e502
noonyuu <noon3m.op.sh@gmail.com>
2026-03-06 10:54:02 +0900

    fix: urlマッピング

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/universal-link-web",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
